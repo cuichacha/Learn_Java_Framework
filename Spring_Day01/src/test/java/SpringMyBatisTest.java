53323b6868321dd39bf81f9031b0625307b2c7be
Will Dufresne <chxdtc123@gmail.com>
2020-12-01 17:25:30 +0800

    another commit of integrated case-study learning

--- Spring_Day01/src/test/java/SpringMyBatisTest.java
+++ Spring_Day01/src/test/java/SpringMyBatisTest.java
@@ -8,10 +8,18 @@ import java.util.List;
 
 public class SpringMyBatisTest {
     @Test
-    public void test() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("SpringJDBC.xml");
+    public void test1() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("SpringJDBC1.xml");
         AccountService accountService = (AccountService) applicationContext.getBean("accountService");
         List<Account> accounts = accountService.findAll();
+        System.out.println(accounts);
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("SpringJDBC2.xml");
+        code2.service.AccountService accountService = (code2.service.AccountService) applicationContext.getBean("accountService");
+        List<code2.domain.Account> accounts = accountService.findAll();
         System.out.println(accounts);
     }
 }
