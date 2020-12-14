740b192347f3fc97b8ed8b09cb99e2b4f30bab60
Will Dufresne <chxdtc123@gmail.com>
2020-12-14 11:46:04 +0800

    another commit

--- SpringBoot_Day01/src/test/java/SpringBootTest.java
+++ SpringBoot_Day01/src/test/java/SpringBootTest.java
@@ -2,12 +2,16 @@ import code.HelloApplication;
 import code.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @org.springframework.boot.test.context.SpringBootTest(classes = HelloApplication.class)
 public class SpringBootTest {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     public void test1() {
@@ -17,5 +21,20 @@ public class SpringBootTest {
     @Test
     public void test2() {
         System.out.println(userService.findAll());
+    }
+
+    @Test
+    public void test3() {
+        redisTemplate.boundValueOps("name").set("123");
+    }
+
+    @Test
+    public void test4() {
+        System.out.println(redisTemplate.opsForValue().get("name"));
+    }
+
+    @Test
+    public void test5() {
+        redisTemplate.delete("name");
     }
 }
